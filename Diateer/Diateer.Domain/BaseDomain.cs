28781c91189c95108307e3cfd7b1b06bf1624c9c
Tomasz Jaworski <tom.jawo@gmail.com>
2018-10-27 22:00:17 +0200

    Dodaj pliki projektów.

---
+++ Diateer/Diateer.Domain/BaseDomain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Diateer.Domain
+{
+    public class BaseDomain
+    {
+        public int Id { get; set; }
+    }
+}
